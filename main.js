d9c17704f1dc57a1408b8e7570c1a30c1b5228c5
herokudev <ao43085@gmail.com>
2021-08-11 16:55:16 -0600

    Fixing linter errors
    Fixing linter errors

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ const menuContainer = document.querySelector('.menu-container');
 const navList = document.querySelector('.nav-list');
 
 menuIcon.addEventListener('click', () => {
-    closeBtn.style.display = 'block';
-    menuContainer.style.height = '500px';
-    navList.style.display = 'block';
-  });
+  closeBtn.style.display = 'block';
+  menuContainer.style.height = '500px';
+  navList.style.display = 'block';
+});
 
-  closeBtn.addEventListener('click', () => {
-    closeBtn.style.display = 'none';
-    menuContainer.style.height = '0px';
-    navList.style.display = 'none';
-  });  
+closeBtn.addEventListener('click', () => {
+  closeBtn.style.display = 'none';
+  menuContainer.style.height = '0px';
+  navList.style.display = 'none';
+});
 
-  navList.addEventListener('click', () => {
-    closeBtn.style.display = 'none';
-    menuContainer.style.height = '0px';
-    navList.style.display = 'none';
-  });  
+navList.addEventListener('click', () => {
+  closeBtn.style.display = 'none';
+  menuContainer.style.height = '0px';
+  navList.style.display = 'none';
+});
